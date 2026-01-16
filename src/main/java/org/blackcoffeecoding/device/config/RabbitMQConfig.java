9158b545ea6f96e80c8dc744ec94988553cb03f9
BlackCoffeeCoding <kvsolntsev@gmail.com>
2026-01-16 13:11:05 +0300

    lab 7 stage

--- src/main/java/org/blackcoffeecoding/device/config/RabbitMQConfig.java
+++ src/main/java/org/blackcoffeecoding/device/config/RabbitMQConfig.java
@@ -1,6 +1,10 @@
 package org.blackcoffeecoding.device.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,10 +12,32 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfig {
     public static final String EXCHANGE_NAME = "devices-exchange";
     public static final String ROUTING_KEY_CREATED = "device.created";
-    public static final String ROUTING_KEY_DELETED = "device.deleted"; // Для самост. работы
+    public static final String ROUTING_KEY_DELETED = "device.deleted";
 
     @Bean
     public TopicExchange devicesExchange() {
-        return new TopicExchange(EXCHANGE_NAME);
+        // durable=true (true, false) означает, что обменник переживет перезагрузку RabbitMQ [cite: 833-834]
+        return new TopicExchange(EXCHANGE_NAME, true, false);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        // Используем Jackson для красивого JSON
+        return new Jackson2JsonMessageConverter(new ObjectMapper().findAndRegisterModules());
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, Jackson2JsonMessageConverter messageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+
+        // Callback: Сработает, когда брокер подтвердит (или отвергнет) получение сообщения [cite: 844-849]
+        rabbitTemplate.setConfirmCallback((correlationData, ack, cause) -> {
+            if (!ack) {
+                System.err.println("NACK: Сообщение не доставлено брокеру! Причина: " + cause);
+            }
+        });
+
+        return rabbitTemplate;
     }
 }
